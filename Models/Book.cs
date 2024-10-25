caf14f9f34f7db679c777085d0896209bad56fed
Larisa Teodorescu <larisateo2010@gmail.com>
2024-10-25 22:07:09 +0300

    updated db with new functionalities

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,10 +1,21 @@
-﻿namespace LAB_2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LAB_2.Models
 {
     public class Book
     {
         public int ID { get; set; }
+
+        [Display(Name = "Book Title")]
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
+
         public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
     }
 }
